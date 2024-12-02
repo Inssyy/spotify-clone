07a63db1ded2ef021cd6553fe16504e0a9df0f1c
Inssyy <ina45392@gmail.com>
2024-12-02 20:06:41 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [/** @type {import('tailwindcss').Config} */
+   
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+      
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
